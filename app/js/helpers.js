09a3d8bce7354fd6449865b0160f7015075b1e73
Eugene Gekhter <egekhter@gmail.com>
2024-09-29 17:43:27 -0500

    use white font on background color helper method

--- app/js/helpers.js
+++ app/js/helpers.js
@@ -138,34 +138,17 @@ function colorDeltaE (rgbA, rgbB) {
     return i < 0 ? 0 : Math.sqrt(i);
 }
 
-function colorRgb2Lab (rgb){
-    let r = rgb[0] / 255, g = rgb[1] / 255, b = rgb[2] / 255, x, y, z;
-    r = (r > 0.04045) ? Math.pow((r + 0.055) / 1.055, 2.4) : r / 12.92;
-    g = (g > 0.04045) ? Math.pow((g + 0.055) / 1.055, 2.4) : g / 12.92;
-    b = (b > 0.04045) ? Math.pow((b + 0.055) / 1.055, 2.4) : b / 12.92;
-    x = (r * 0.4124 + g * 0.3576 + b * 0.1805) / 0.95047;
-    y = (r * 0.2126 + g * 0.7152 + b * 0.0722) / 1.00000;
-    z = (r * 0.0193 + g * 0.1192 + b * 0.9505) / 1.08883;
-    x = (x > 0.008856) ? Math.pow(x, 1/3) : (7.787 * x) + 16/116;
-    y = (y > 0.008856) ? Math.pow(y, 1/3) : (7.787 * y) + 16/116;
-    z = (z > 0.008856) ? Math.pow(z, 1/3) : (7.787 * z) + 16/116;
-    return [(116 * y) - 16, 500 * (x - y), 200 * (y - z)]
-}
+function useWhiteOnBackground(hexColor) {
+    // Remove the # if it's there
+    hexColor = hexColor.replace('#', '');
 
-function colorHexToRgb (hex) {
-    var r = parseInt(hex.slice(1, 3), 16),
-        g = parseInt(hex.slice(3, 5), 16),
-        b = parseInt(hex.slice(5, 7), 16);
+    // Convert hex to RGB
+    const r = parseInt(hexColor.substr(0, 2), 16);
+    const g = parseInt(hexColor.substr(2, 2), 16);
+    const b = parseInt(hexColor.substr(4, 2), 16);
 
-    return [r, g, b];
-}
+    // Calculate luminance
+    const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
 
-function presentWhiteFont(color_hex) {
-    let colorFromData = colorHexToRgb('#000000');
-
-    let colorToData = colorHexToRgb(color_hex);
-
-    const distance = colorDeltaE(colorFromData, colorToData);
-
-    return distance < 70;
+    return luminance < 0.5 ? true : false;
 }
