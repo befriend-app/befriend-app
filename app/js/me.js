c0c50ea3fecbd1420b150141d77a33f45d367cd9
egekhter <egekhter@gmail.com>
2024-10-30 16:52:22 -0500

    me: add option to sections

--- app/js/me.js
+++ app/js/me.js
@@ -93,6 +93,28 @@ befriend.me = {
             resolve();
         });
     },
+    addSection: function (key) {
+        let option_data = befriend.me.sections.options[key];
+
+        let sections_el = befriend.els.me.querySelector('.about-me').querySelector('.sections');
+
+        if(option_data) {
+            if(!(key in befriend.me.sections.active)) {
+                befriend.me.sections.active[key] = structuredClone(option_data);
+
+                let html = `<div class="section" data-key="${key}">
+                                <div class="section-top">
+                                    <div class="icon">${option_data.icon}</div>
+                                    <div class="title">${option_data.name}</div>
+                                    <div class="delete"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 346.8033 427.0013"><path d="M232.4016,154.7044c-5.5234,0-10,4.4766-10,10v189c0,5.5195,4.4766,10,10,10s10-4.4805,10-10v-189c0-5.5234-4.4766-10-10-10Z"/><path d="M114.4016,154.7044c-5.5234,0-10,4.4766-10,10v189c0,5.5195,4.4766,10,10,10s10-4.4805,10-10v-189c0-5.5234-4.4766-10-10-10Z"/><path d="M28.4016,127.1224v246.3789c0,14.5625,5.3398,28.2383,14.668,38.0508,9.2852,9.8398,22.207,15.4258,35.7305,15.4492h189.2031c13.5273-.0234,26.4492-5.6094,35.7305-15.4492,9.3281-9.8125,14.668-23.4883,14.668-38.0508V127.1224c18.543-4.9219,30.5586-22.8359,28.0781-41.8633-2.4844-19.0234-18.6914-33.2539-37.8789-33.2578h-51.1992v-12.5c.0586-10.5117-4.0977-20.6055-11.5391-28.0312C238.4212,4.0482,228.3118-.0846,217.8001.0013h-88.7969c-10.5117-.0859-20.6211,4.0469-28.0625,11.4688-7.4414,7.4258-11.5977,17.5195-11.5391,28.0312v12.5h-51.1992c-19.1875.0039-35.3945,14.2344-37.8789,33.2578-2.4805,19.0273,9.5352,36.9414,28.0781,41.8633ZM268.0032,407.0013H78.8001c-17.0977,0-30.3984-14.6875-30.3984-33.5v-245.5h250v245.5c0,18.8125-13.3008,33.5-30.3984,33.5ZM109.4016,39.5013c-.0664-5.207,1.9805-10.2188,5.6758-13.8945,3.6914-3.6758,8.7148-5.6953,13.9258-5.6055h88.7969c5.2109-.0898,10.2344,1.9297,13.9258,5.6055,3.6953,3.6719,5.7422,8.6875,5.6758,13.8945v12.5H109.4016v-12.5ZM38.2024,72.0013h270.3984c9.9414,0,18,8.0586,18,18s-8.0586,18-18,18H38.2024c-9.9414,0-18-8.0586-18-18s8.0586-18,18-18Z"/><path d="M173.4016,154.7044c-5.5234,0-10,4.4766-10,10v189c0,5.5195,4.4766,10,10,10s10-4.4805,10-10v-189c0-5.5234-4.4766-10-10-10Z"/></svg></div>
+                                </div>
+                            </div>`;
+
+                sections_el.insertAdjacentHTML('beforeend', html);
+
+            }
+        }
+    },
     setOptions: function () {
         let html = ``;
 
@@ -130,6 +152,7 @@ befriend.me = {
             });
         },
         onAddSection: function () {
+            //open
             let btn_el = befriend.els.me.querySelector('.add-section-btn');
 
             btn_el.addEventListener('click', function (e) {
@@ -142,6 +165,30 @@ befriend.me = {
                      befriend.me.toggleSectionOptions(true);
                  }
             });
+
+            //add
+            let options = befriend.els.meSectionOptions.getElementsByClassName('option');
+
+            for(let i = 0; i < options.length; i++) {
+                let option = options[i];
+
+                if(option._listener) {
+                    continue;
+                }
+
+                option._listener = true;
+
+                option.addEventListener('click', function (e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+
+                    let key = this.getAttribute('data-key');
+
+                    befriend.me.addSection(key);
+
+                    this.parentNode.removeChild(this);
+                });
+            }
         }
     }
 };
