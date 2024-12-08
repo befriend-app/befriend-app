e6fdfe666fb4bdb2d9a1f8f0d1a9d08701f0abe4
egekhter <egekhter@gmail.com>
2024-12-08 12:28:09 -0600

    position verification toggle

--- app/js/filters.js
+++ app/js/filters.js
@@ -2477,8 +2477,10 @@ befriend.filters = {
             const html = `
                 <div class="filter-options-container">
                     <div class="filter-option verification-button" data-filter-token="verification_in_person">
+                        ${befriend.filters.sendReceiveHtml(true, true)}
+
                         <div class="content">
-                            <div class="icon">
+                            <div class="verification-icon">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                                     <path d="M256,0C114.6,0,0,114.6,0,256s114.6,256,256,256s256-114.6,256-256S397.4,0,256,0z M256,128c39.8,0,72,32.2,72,72 s-32.2,72-72,72s-72-32.2-72-72S216.2,128,256,128z M256,448c-52.9,0-100.3-23.1-133.2-59.6c26.1-42.4,72.5-70.4,125.2-70.4 c2.7,0,5.3,0.1,7.9,0.3c2.6-0.2,5.2-0.3,7.9-0.3c52.7,0,99.1,28,125.2,70.4C356.3,424.9,308.9,448,256,448z"/>
                                 </svg>
@@ -2487,12 +2489,14 @@ befriend.filters = {
                                 <div class="name">In-Person</div>
                             </div>
 
-                            ${toggleHtml(true, null, 'toggle-24')}
+                            ${toggleHtml(true, 'On', 'toggle-24')}
                         </div>
                     </div>
                     <div class="filter-option verification-button" data-filter-token="verification_linkedin">
+                        ${befriend.filters.sendReceiveHtml(true, true)}
+
                         <div class="content">
-                            <div class="icon">
+                            <div class="verification-icon">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                                     <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/>
                                 </svg>
@@ -2501,7 +2505,7 @@ befriend.filters = {
                                 <div class="name">LinkedIn</div>
                             </div>
                             
-                            ${toggleHtml(true, null, 'toggle-24')}
+                            ${toggleHtml(true, 'On', 'toggle-24')}
 
                         </div>
                     </div>
